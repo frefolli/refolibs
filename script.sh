a7f753c6b4db204785510d36fe7c06f62d94d00e
Francesco Refolli <francesco.refolli@binarysystem.eu>
2023-09-26 09:02:05 +0200

    update + script.sh

---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+REPOS="libclipp  libgc  libhyper  liblexer  libparser  libutils"
+THIS_PATH=$(dirname "$0")
+
+function download() {
+    if [[ "$THIS_PATH" == '.' ]]; then
+        echo -e "cannot execute $0 from it's own directory"
+    else
+        for repo in $REPOS; do
+            git clone git@github.com:frefolli/$repo
+        done
+    fi
+}
+
+if [ ! -z "$1" ]; then
+    if [[ "$1" == 'download' ]]; then
+        download
+    fi
+else
+    echo -e "choose an action [download | ...]"
+fi
